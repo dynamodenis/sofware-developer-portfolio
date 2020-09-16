4dfb728d767bc026dd735a0bd7f26135e63cd05c
dynamodenis <dmbugua66@gmail.com>
2020-09-16 12:57:57 +0300

    skills created

--- portfolio_website/settings.py
+++ portfolio_website/settings.py
@@ -51,8 +51,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
 ]
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
