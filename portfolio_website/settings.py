facce1a1c267a03fa7f90cbe80b3fafdc4080414
dynamodenis <dmbugua66@gmail.com>
2020-09-28 20:18:13 +0300

    removed site

--- portfolio_website/settings.py
+++ portfolio_website/settings.py
@@ -51,7 +51,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites',
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
